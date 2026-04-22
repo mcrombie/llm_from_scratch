e0add7f166b5727c486175cf25d054b96df4b44f
Michael Crombie <mcrombie1994@gmail.com>
2026-04-22 07:35:35 -0400

    2.2 Tokenizing Text
    Splitting input text into individual tokens.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from llm-from-scratch!")
+import urllib.request, re
+url = (
+    "https://raw.githubusercontent.com/rasbt/"
+       "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+       "the-verdict.txt"
+) 
+file_path = "the-verdict.txt" 
+urllib.request.urlretrieve(url, file_path)
 
+
+
+def main():
+    with open("the-verdict.txt", "r", encoding="utf-8") as f: 
+        raw_text = f.read() 
+    print("Total number of character:", len(raw_text)) 
+    print(raw_text[:99])
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text) 
+    preprocessed = [item.strip() for item in preprocessed if item.strip()] 
+    print(len(preprocessed))
+    print(preprocessed[:30])
 
 if __name__ == "__main__":
     main()
