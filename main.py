05e16248ba25f2365eadc6035b613d20cac14581
Michael Crombie <mcrombie1994@gmail.com>
2026-04-23 08:46:09 -0400

    2.3 Converting Tokens into token IDs
    Converting text into numerical tokens

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ url = (
 file_path = "the-verdict.txt" 
 urllib.request.urlretrieve(url, file_path)
 
-
+class SimpleTokenizerV1: 
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
 
 def main():
     with open("the-verdict.txt", "r", encoding="utf-8") as f: 
@@ -16,8 +29,20 @@ def main():
     print(raw_text[:99])
     preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text) 
     preprocessed = [item.strip() for item in preprocessed if item.strip()] 
-    print(len(preprocessed))
-    print(preprocessed[:30])
+    all_words = sorted(set(preprocessed)) 
+    vocab_size = len(all_words) 
+    print(vocab_size)
+    vocab = {token:integer for integer,token in enumerate(all_words)} 
+    for i, item in enumerate(vocab.items()):
+        print(item)
+        if i >= 50:
+            break
+
+    tokenizer = SimpleTokenizerV1(vocab) 
+    text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride.""" 
+    ids = tokenizer.encode(text) 
+    print(ids)
+    print(tokenizer.decode(ids))
 
 if __name__ == "__main__":
     main()
