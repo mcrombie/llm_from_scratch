64e61ca117b119d6624851fb19788c8f5ca3e129
Michael Crombie <mcrombie1994@gmail.com>
2026-04-24 07:06:06 -0400

    2.4 Adding special context tokens
    Added tokens for unknown characters and end of text

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ url = (
 file_path = "the-verdict.txt" 
 urllib.request.urlretrieve(url, file_path)
 
-class SimpleTokenizerV1: 
+class SimpleTokenizerV2: 
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
+
     def encode(self, text):
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
         
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 def main():
@@ -38,11 +41,27 @@ def main():
         if i >= 50:
             break
 
-    tokenizer = SimpleTokenizerV1(vocab) 
+    tokenizer = SimpleTokenizerV2(vocab) 
     text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride.""" 
     ids = tokenizer.encode(text) 
     print(ids)
     print(tokenizer.decode(ids))
+
+    all_tokens = sorted(list(set(preprocessed)))
+    all_tokens.extend(["<|endoftext|>", "<|unk|>"]) 
+    vocab = {token:integer for integer,token in enumerate(all_tokens)} 
+    print(len(vocab.items()))
+    for i, item in enumerate(list(vocab.items())[-5:]): 
+        print(item)
+
+    
+    text1 = "Hello, do you like tea?" 
+    text2 = "In the sunlit terraces of the palace." 
+    text = " <|endoftext|> ".join((text1, text2)) 
+    print(text) 
+    tokenizer = SimpleTokenizerV2(vocab) 
+    print(tokenizer.encode(text))
+    print(tokenizer.decode(tokenizer.encode(text)))
 
 if __name__ == "__main__":
     main()
