17866c7b089c87170deb3c3ab871af5e31678c56
Michael Crombie <mcrombie1994@gmail.com>
2026-04-26 08:31:32 -0400

    2.6 Data sampling with a sliding window
    Learning about tensors for inputs and outputs and using parameters like max_length and stride to influence data loading. Basically introducing the fixed number of tokens a model can see at once (the context window).

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ urllib.request.urlretrieve(url, file_path)
 from importlib.metadata import version 
 import tiktoken
 
+
+import torch 
+from torch.utils.data import Dataset, DataLoader
+
 class SimpleTokenizerV2: 
     def __init__(self, vocab):
         self.str_to_int = vocab
@@ -27,6 +31,27 @@ class SimpleTokenizerV2:
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
+    
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+    def __len__(self):
+        return len(self.input_ids)
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0): 
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+    return dataloader
 
 def main():
     with open("the-verdict.txt", "r", encoding="utf-8") as f: 
@@ -72,6 +97,43 @@ def main():
     print(integers)
     strings = tokenizer.decode(integers) 
     print(strings)
+
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read() 
+    enc_text = tokenizer.encode(raw_text) 
+    print(len(enc_text))
+    enc_sample = enc_text[50:]
+    context_size = 4
+    x = enc_sample[:context_size] 
+    y = enc_sample[1:context_size+1]
+    print(f"x: {x}") 
+    print(f"y: {y}")
+
+    for i in range(1, context_size+1):
+        context = enc_sample[:i]
+        desired = enc_sample[i]
+        print(context, "---->", desired)
+
+    for i in range(1, context_size+1):
+        context = enc_sample[:i]
+        desired = enc_sample[i]
+        print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read() 
+    
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=1, max_length=8, stride=2, shuffle=False) 
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter) 
+    print(first_batch)
+    second_batch = next(data_iter) 
+    print(second_batch)
+    dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False )
+    data_iter = iter(dataloader) 
+    inputs, targets = next(data_iter) 
+    print("Inputs:\n", inputs) 
+    print("\nTargets:\n", targets)
 
 if __name__ == "__main__":
     main()
