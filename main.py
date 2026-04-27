0c7f2a891076aeb7568c3f7313f27a4ebdcddcd7
Michael Crombie <mcrombie1994@gmail.com>
2026-04-27 08:00:05 -0400

    2.7 Creating token embeddings
    Creating the embedding layer

--- main.py
+++ main.py
@@ -135,5 +135,15 @@ def main():
     print("Inputs:\n", inputs) 
     print("\nTargets:\n", targets)
 
+    input_ids = torch.tensor([2, 3, 5, 1])
+    vocab_size = 6 
+    output_dim = 3 
+    torch.manual_seed(123) 
+    embedding_layer = torch.nn.Embedding(vocab_size, output_dim) 
+    print(embedding_layer.weight)
+    print(embedding_layer(torch.tensor([3])))
+
+    
+
 if __name__ == "__main__":
     main()
