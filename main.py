e3a2af0470379f39c8a9a59399fafb94ca944515
Michael Crombie <mcrombie1994@gmail.com>
2026-04-28 09:05:53 -0400

    2.8 Encoding word positions
    Positionally aware embeddings

--- main.py
+++ main.py
@@ -141,8 +141,23 @@ def main():
     torch.manual_seed(123) 
     embedding_layer = torch.nn.Embedding(vocab_size, output_dim) 
     print(embedding_layer.weight)
-    print(embedding_layer(torch.tensor([3])))
+    print(embedding_layer(torch.tensor([3])))   
 
+    vocab_size = 50257 
+    output_dim = 256 
+    token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    max_length = 4 
+    dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False ) 
+    data_iter = iter(dataloader) 
+    inputs, targets = next(data_iter) 
+    print("Token IDs:\n", inputs) 
+    print("\nInputs shape:\n", inputs.shape) 
+    token_embeddings = token_embedding_layer(inputs) 
+    print(token_embeddings.shape)
+    context_length = max_length 
+    pos_embedding_layer = torch.nn.Embedding(context_length, output_dim) 
+    pos_embeddings = pos_embedding_layer(torch.arange(context_length)) 
+    print(pos_embeddings.shape) 
     
 
 if __name__ == "__main__":
