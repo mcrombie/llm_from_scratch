36e9c1c9cf3f7f1144b0ad59054f7684ab852dc0
Michael Crombie <mcrombie1994@gmail.com>
2026-05-03 07:03:59 -0400

    3.4.2 Implementing a compact self-attention Python class
    classes for self-attention. Better version uses matrix multiplication

--- main.py
+++ main.py
@@ -19,6 +19,38 @@ FILE_PATH = "the-verdict.txt"
 if not os.path.exists(FILE_PATH):
     urllib.request.urlretrieve(URL, FILE_PATH)
 
+import torch.nn as nn 
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+    
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
 
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
@@ -194,6 +226,14 @@ def main():
 
     context_vec_2 = attn_weights_2 @ values 
     print(context_vec_2)
+
+    torch.manual_seed(123) 
+    sa_v1 = SelfAttention_v1(d_in, d_out) 
+    print(sa_v1(inputs))
+
+    torch.manual_seed(789) 
+    sa_v2 = SelfAttention_v2(d_in, d_out) 
+    print(sa_v2(inputs))
 
 if __name__ == "__main__":
     main()
