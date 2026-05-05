056f19b3d21a93a3048e1769fde43cf18aaa10ec
Michael Crombie <mcrombie1994@gmail.com>
2026-05-05 06:35:31 -0400

    3.5.2 Masking additional attention weights with dropout
    randomly selected hidden layer units are ignored during training, effectively “dropping” them out.

--- main.py
+++ main.py
@@ -264,6 +264,13 @@ def main():
     attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1) 
     print(attn_weights)
 
+    torch.manual_seed(123) 
+    dropout = torch.nn.Dropout(0.5)
+    example = torch.ones(6, 6)
+    print(dropout(example))
+
+    torch.manual_seed(123) 
+    print(dropout(attn_weights))
 
 if __name__ == "__main__":
     main()
