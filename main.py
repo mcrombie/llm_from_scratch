7ec963c34b902e9eeee83b8c648684b1f93215f8
Michael Crombie <mcrombie1994@gmail.com>
2026-05-06 07:08:32 -0400

    3.5.2 Masking additional weights with dropout
    Another dropout example

--- main.py
+++ main.py
@@ -272,5 +272,13 @@ def main():
     torch.manual_seed(123) 
     print(dropout(attn_weights))
 
+    torch.manual_seed(123) 
+    dropout = torch.nn.Dropout(0.5)
+    example = torch.ones(6, 6)
+    print(dropout(example))
+
+    torch.manual_seed(123) 
+    print(dropout(attn_weights)) 
+
 if __name__ == "__main__":
     main()
