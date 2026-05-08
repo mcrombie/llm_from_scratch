131d1d35f7e870efd19ce3e3314f375575029840
Michael Crombie <mcrombie1994@gmail.com>
2026-05-08 07:22:23 -0400

    3.6 Extending single-head attention to multi-head attention; 3.6.1
    running the attention method multiple times in parallel with different, learned linear projections.

--- main.py
+++ main.py
@@ -73,6 +73,14 @@ class CausalAttention(nn.Module):
         attn_weights = self.dropout(attn_weights)
         context_vec = attn_weights @ values 
         return context_vec
+    
+
+class MultiHeadAttentionWrapper(nn.Module): 
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList([CausalAttention(d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_heads)])
+    
+    def forward(self, x):return torch.cat([head(x) for head in self.heads], dim=-1)
 
 
 def main():
@@ -311,6 +319,15 @@ def main():
     ca = CausalAttention(d_in, d_out, context_length, 0.0) 
     context_vecs = ca(batch) 
     print("context_vecs.shape:", context_vecs.shape)
+
+    torch.manual_seed(123) 
+    context_length = batch.shape[1] # This is the number of tokens 
+    d_in, d_out = 3, 1
+    mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2 ) 
+    context_vecs = mha(batch) 
+    print(context_vecs) 
+    print("context_vecs.shape:", context_vecs.shape)
+
 
 if __name__ == "__main__":
     main()
