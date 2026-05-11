fe198f8e276d06be026a567b6c9f26b44aedd789
Michael Crombie <mcrombie1994@gmail.com>
2026-05-11 11:12:51 -0400

    4.1 ErrorFix
    I just missed adding GPT_CONFIG_124M defined earlier in the chapter and needed to fix the location of the forward function in the DummyGPTModel class

--- main.py
+++ main.py
@@ -125,6 +125,16 @@ class MultiHeadAttention(nn.Module):
         return context_vec
 
 
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size     
+    "context_length": 1024, # Context length     
+    "emb_dim": 768, # Embedding dimension     
+    "n_heads": 12, # Number of attention heads     
+    "n_layers": 12, # Number of layers     
+    "drop_rate": 0.1, # Dropout rate     
+    "qkv_bias": False # Query-Key-Value bias 
+    }
+
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
@@ -138,16 +148,16 @@ class DummyGPTModel(nn.Module):
         self.final_norm = DummyLayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
         
-        def forward(self, in_idx): 
-            batch_size, seq_len = in_idx.shape 
-            tok_embeds = self.tok_emb(in_idx)
-            pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
-            x = tok_embeds + pos_embeds
-            x = self.drop_emb(x)
-            x = self.trf_blocks(x)
-            x = self.final_norm(x)
-            logits = self.out_head(x)
-            return logits
+    def forward(self, in_idx): 
+        batch_size, seq_len = in_idx.shape 
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
         
 class DummyTransformerBlock(nn.Module):
     def __init__(self, cfg):
@@ -434,15 +444,6 @@ def main():
     batch = torch.stack(batch, dim=0) 
     print(batch)
 
-    GPT_CONFIG_124M = {
-    "vocab_size": 50257,
-    "context_length": 1024,
-    "emb_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
-    "drop_rate": 0.1,
-    "qkv_bias": False
-    }
 
     torch.manual_seed(123) 
     model = DummyGPTModel(GPT_CONFIG_124M) 
