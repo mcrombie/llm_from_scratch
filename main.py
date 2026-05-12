2bd88a65e950721bb9addada3b7f9cf30dfd614e
Michael Crombie <mcrombie1994@gmail.com>
2026-05-12 07:45:58 -0400

    4.2 Normalizing activations with layer normalization
    the chain rule is a way to compute gradients of a loss function given the model’s parameters in a computation graph. PyTorch takes care of the calculus for via the .backward method.

--- main.py
+++ main.py
@@ -171,6 +171,19 @@ class DummyLayerNorm(nn.Module):
     def forward(self, x):
         return x 
     
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5 
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift 
+    
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
         raw_text = f.read()
@@ -450,6 +463,37 @@ def main():
     logits = model(batch) 
     print("Output shape:", logits.shape) 
     print(logits)
+
+
+    torch.manual_seed(123) 
+    batch_example = torch.randn(2, 5)
+    layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU()) 
+    out = layer(batch_example) 
+    print(out)
+
+    mean = out.mean(dim=-1, keepdim=True) 
+    var = out.var(dim=-1, keepdim=True)
+    print("Mean:\n", mean) 
+    print("Variance:\n", var)
+
+    out_norm = (out - mean) / torch.sqrt(var) 
+    mean = out_norm.mean(dim=-1, keepdim=True) 
+    var = out_norm.var(dim=-1, keepdim=True) 
+    print("Normalized layer outputs:\n", out_norm) 
+    print("Mean:\n", mean) 
+    print("Variance:\n", var)
+
+    torch.set_printoptions(sci_mode=False) 
+    print("Mean:\n", mean) 
+    print("Variance:\n", var)
+
+    ln = LayerNorm(emb_dim=6) 
+    out_ln = ln(out) 
+    mean = out_ln.mean(dim=-1, keepdim=True) 
+    var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+    print("Mean:\n", mean) 
+    print("Variance:\n", var)
+
 
 if __name__ == "__main__":
     main()
