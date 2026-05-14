1db522b98f1d602168f6e60cb863470ef5370adf
Michael Crombie <mcrombie1994@gmail.com>
2026-05-14 07:30:27 -0400

    4.4 Adding shortcut connections
    Implementing a deep neural network with shortcut connections.

--- main.py
+++ main.py
@@ -206,6 +206,36 @@ class FeedForward(nn.Module):
         return self.layers(x) 
     
 
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), 
+                          GELU()), 
+                          nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), GELU()), 
+                          nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), GELU()), 
+                          nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), GELU()), 
+                          nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), GELU())]) 
+    def forward(self, x): 
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else: 
+                x = layer_output
+        return x
+    
+
+def print_gradients(model, x):
+    output = model(x) 
+    target = torch.tensor([[0.]])
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+    loss.backward()
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}") 
     
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
@@ -519,24 +549,36 @@ def main():
 
     gelu, relu = GELU(), nn.ReLU() 
     
-    x = torch.linspace(-3, 3, 100)
-    y_gelu, y_relu = gelu(x), relu(x) 
-    plt.figure(figsize=(8, 3)) 
-    for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
-        plt.subplot(1, 2, i)
-        plt.plot(x, y) 
-        plt.title(f"{label} activation function")
-        plt.xlabel("x")
-        plt.ylabel(f"{label}(x)")
-        plt.grid(True) 
-        plt.tight_layout() 
-        plt.show()
+    # x = torch.linspace(-3, 3, 100)
+    # y_gelu, y_relu = gelu(x), relu(x) 
+    # plt.figure(figsize=(8, 3)) 
+    # for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    #     plt.subplot(1, 2, i)
+    #     plt.plot(x, y) 
+    #     plt.title(f"{label} activation function")
+    #     plt.xlabel("x")
+    #     plt.ylabel(f"{label}(x)")
+    #     plt.grid(True) 
+    #     plt.tight_layout() 
+    #     plt.show()
 
 
     ffn = FeedForward(GPT_CONFIG_124M) 
     x = torch.rand(2, 3, 768)
     out = ffn(x) 
     print(out.shape)
+
+    layer_sizes = [3, 3, 3, 3, 3, 1]
+    sample_input = torch.tensor([[1., 0., -1.]]) 
+    torch.manual_seed(123)
+    model_without_shortcut = ExampleDeepNeuralNetwork( layer_sizes, use_shortcut=False )
+
+    print_gradients(model_without_shortcut, sample_input)
+
+
+    torch.manual_seed(123) 
+    model_with_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=True ) 
+    print_gradients(model_with_shortcut, sample_input)
 
 if __name__ == "__main__":
     main()
