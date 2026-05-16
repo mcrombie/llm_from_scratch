25fa8db14e32cc622f9800d7f2e9a8a973b94623
Michael Crombie <mcrombie1994@gmail.com>
2026-05-16 10:47:53 -0400

    4.6 Coding the GPT model
    Initializing a 124-million parameter GPT model ("GPT-2 small"). Basically replaced the dummy transformer and layernorm classes with the real ones to test a working version.

--- main.py
+++ main.py
@@ -267,6 +267,29 @@ class TransformerBlock(nn.Module):
         x = x + shortcut
         return x
     
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]) 
+        self.final_norm = LayerNorm(cfg["emb_dim"]) 
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape 
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits 
+    
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
         raw_text = f.read()
@@ -617,6 +640,27 @@ def main():
     output = block(x) 
     print("Input shape:", x.shape) 
     print("Output shape:", output.shape)
+
+    torch.manual_seed(123) 
+    model = GPTModel(GPT_CONFIG_124M) 
+    out = model(batch) 
+    print("Input batch:\n", batch) 
+    print("\nOutput shape:", out.shape) 
+    print(out)
+
+    total_params = sum(p.numel() for p in model.parameters()) 
+    print(f"Total number of parameters: {total_params:,}")
+
+    print("Token embedding layer shape:", model.tok_emb.weight.shape) 
+    print("Output layer shape:", model.out_head.weight.shape)
+    total_params_gpt2 = (
+        total_params - sum(p.numel()
+        for p in model.out_head.parameters()) ) 
+    print(f"Number of trainable parameters "f"considering weight tying: {total_params_gpt2:,}" )
+
+    total_size_bytes = total_params * 4
+    total_size_mb = total_size_bytes / (1024 * 1024)
+    print(f"Total size of the model: {total_size_mb:.2f} MB")
 
 if __name__ == "__main__":
     main()
